c569018fc3588b4b231f7ab0c8a7a4f3d918eb88
Carlos Rios <carlosriospontes@gmail.com>
2022-06-12 15:33:49 -0300

    task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
+use App\Models\Tag;
+use App\Models\Task;
 use Illuminate\Http\Request;
+
+use function GuzzleHttp\Promise\all;
 
 class TaskController extends Controller
 {
@@ -13,17 +18,14 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $qntTask = Task::count('id');
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        for ($i=1; $i <= $qntTask ; $i++) {
+
+            $task[] = Task::with('tag')->findOrFail($i);
+        }
+
+        return response()->json($task, 200);
     }
 
     /**
@@ -32,9 +34,11 @@ class TaskController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        //
+        $task = Task::create($request->all());
+
+        return response()->json($task, 200);
     }
 
     /**
@@ -49,26 +53,26 @@ class TaskController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update($id, TaskRequest $request)
     {
-        //
+        try {
+
+            $task = Task::findOrFail($id);
+
+            $task->update($request->all());
+            $task->save();
+
+            return response()->json($task, 200);
+
+        } catch (\Throwable $th) {
+            return response()->json(['erro' => "Tarefa com id $id não escontrada no banco de dados"], 404);
+        }
     }
 
     /**
@@ -80,5 +84,25 @@ class TaskController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function updateStatus($id)
+    {
+        try {
+            $task = Task::findOrFail($id);
+
+            if ($task->status < 3){
+                $task->update([
+                    'status' => $task->status + 1
+                ]);
+
+                return response()->json($task, 200);
+            }
+
+            return response()->json(['message' => 'Esta tarefa já está aprovada']);
+
+        } catch (\Throwable $th) {
+            return response()->json(['erro' => "Não foi encontrada nenhuma tarefa com o id $id"]);
+        }
     }
 }
